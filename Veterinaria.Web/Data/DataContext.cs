451306a09e96de9e7a054b9d7dac506f11212eac
Mds Marin desarrollo software <carlos827@yahoo.com>
2020-03-19 15:49:50 +0100

    created table owner

---
+++ Veterinaria.Web/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Veterinaria.Web.Data.Entities;
+
+namespace Veterinaria.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Agenda> Agendas { get; set; }
+        //public DbSet<History> Histories { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        //public DbSet<Manager> Managers { get; set; }
+        //public DbSet<Pet> Pets { get; set; }
+        //public DbSet<PetType> PetTypes { get; set; }
+        //public DbSet<ServiceType> ServiceTypes { get; set; }
+
+    }
+}
--- Veterinaria.Web/Startup.cs
+++ Veterinaria.Web/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Veterinaria.Web.Data;
 
 namespace Veterinaria.Web
 {
@@ -31,8 +33,13 @@ namespace Veterinaria.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<DataContext>(cfg => {
+                cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
